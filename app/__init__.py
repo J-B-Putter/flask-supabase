241bde1c5dee0f5fcb85a10a2e1e24ff2c2a5e22
J-B-Putter <jputter@waimea.school.nz>
2025-05-21 12:25:41 +1200

    some stuff

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import render_template
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+# Get environment variables
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# Connenct to Supabase as a client
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+print(supabase)
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    response = supabase.table("things").select().order("name").execute()
+    records = response.data
+    return render_template("pages/home.jinja", things=records)
+
+@app.get("/thing/<int:id>")
+def showThing(id):
+    response = supabase.table("things").select().eq("id", id).single().execute()
+    record = response.data
+    return render_template("pages/thing.jinja", thing=record)
+
+@app.get("/delete/<int::id>")
+def deleteThing(id):
+    ####
+    return
+
+@app.errorhandler(404)
+def notFound(error):
+    return render_template("pages/404.jinja")
